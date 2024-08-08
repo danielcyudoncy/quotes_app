8decb633f6b7147c03593592e4b7dfd3cabf78a9
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-08-08 16:38:52 +0100

    loginscreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quotes_app/controllers/auth_controller.dart';
 
-class LogInScreen extends StatelessWidget {
-  const LogInScreen({super.key});
+
+class LoginScreen extends StatelessWidget {
+   LoginScreen({super.key});
+  final AuthController controller = Get.put(AuthController());
+
+  
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          const TextField(
+            decoration: InputDecoration(labelText: "Username"),
+          ),
+          const TextField(
+            decoration: InputDecoration(labelText: "Password"),
+            obscureText: true,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              controller.login('username', 'password');
+            },
+            child: const Text("Login"),
+          ),
+        ],
+      ),
+    );
   }
 }
