4f7acab54c5beda420963e1d8cae8c6fbff9daf2
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-08-08 02:36:10 +0100

    updating myApp Screen

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -1,5 +1,29 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:quizztale_app/bindings/bindings.dart';
+import 'package:quizztale_app/routes/routes.dart';
+import 'package:quizztale_app/screens/game_screen.dart';
+import 'package:quizztale_app/screens/game_status.dart';
+import 'package:quizztale_app/screens/splash_screen.dart';
+import 'package:quizztale_app/screens/welcome_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Daily Quotes',
+      getPages: [
+        GetPage(
+            name: Routes.splashScreen, page: () => const SpalshScreen()),
+        GetPage(name: Routes.onboardingScreen, page: () => const OnboardingScreen()),
+        GetPage(name: Routes.signInScreen, page: () => const SignInScreen()),
+        GetPage(name: Routes.RegisterScreen, page: () => const RegisterScreen()),
+        GetPage(name: Routes.ProfileScreen, page: () => const ProfileScreen()),
+      ],
+      home: const SplashScreen(),
+    );
+  }
 }
