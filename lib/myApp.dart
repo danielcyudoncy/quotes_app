56eeab2ff9dc600ff3f380207605c163e557da13
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-08-10 02:44:31 +0100

    update

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:quizztale_app/bindings/bindings.dart';
-import 'package:quizztale_app/routes/routes.dart';
-import 'package:quizztale_app/screens/game_screen.dart';
-import 'package:quizztale_app/screens/game_status.dart';
-import 'package:quizztale_app/screens/splash_screen.dart';
-import 'package:quizztale_app/screens/welcome_screen.dart';
+import 'package:quotes_app/routes/routes.dart';
+
+import 'app_bindings/app_bindings.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,16 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Daily Quotes',
-      getPages: [
-        GetPage(
-            name: Routes.splashScreen, page: () => const SpalshScreen()),
-        GetPage(name: Routes.onboardingScreen, page: () => const OnboardingScreen()),
-        GetPage(name: Routes.signInScreen, page: () => const SignInScreen()),
-        GetPage(name: Routes.RegisterScreen, page: () => const RegisterScreen()),
-        GetPage(name: Routes.ProfileScreen, page: () => const ProfileScreen()),
-      ],
-      home: const SplashScreen(),
+      initialBinding: AppBindings(), 
+      initialRoute: '/splash', 
+      getPages: AppRoutes.routes,
     );
   }
 }
