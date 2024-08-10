30a4f3f4c24881b0e1568ad56289656dd4ae7e74
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-08-10 02:59:19 +0100

    updated

--- lib/utils/validators/validators.dart
+++ lib/utils/validators/validators.dart
@@ -1,44 +1,18 @@
-class AppValidator {
-  static String? validateEmptyText(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Name is required';
-    }
-    return null;
+String? validateEmail(String email) {
+  final RegExp emailRegex = RegExp(
+    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  );
+
+  if (email.isEmpty) {
+    return 'Email is required';
+  } else if (!emailRegex.hasMatch(email)) {
+    return 'Enter a valid email';
   }
-
-  static String? validEmail(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Email is required';
-    }
-    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-    if (!emailRegExp.hasMatch(value)) {
-      return 'Invalid email address';
-    }
-
-    return null;
-  }
-
-  static String? validatePassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Password is required';
-    }
-
-    if (value.length < 6) {
-      return 'Password must be at least 6 Characters long';
-    }
-
-    if (!value.contains(RegExp(r'[A-Z]'))) {
-      return 'Password must contain at least one uppercase letter';
-    }
-
-    if (!value.contains(RegExp(r'[0-9]'))) {
-      return 'Password must contain at least one number';
-    }
-
-    if (!value.contains(RegExp(r'[!@#$%^&*(),.?:{}|<>]'))) {
-      return 'Password must contain at least one special character';
-    }
-
-    return null;
+  return null;
+}
+class ValidEmail {
+  bool validate(String email) {
+    final RegExp regex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return regex.hasMatch(email);
   }
 }
