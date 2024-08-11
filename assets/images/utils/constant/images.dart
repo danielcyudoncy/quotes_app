c597d00c1eec1b5990402d626266dc6b24d8f545
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-08-11 22:19:41 +0100

    updated

--- assets/images/utils/constant/images.dart
+++
@@ -1,5 +0,0 @@
-class AppImages {
-  //app Images
-  static const String splashImage = "assets/images/quote.png";
-  static const String googleImage = "assets/images/google.png";
-}
--- assets/images/utils/constant/text.dart
+++
@@ -1,3 +0,0 @@
-
-
-const appName = 'QuotesApp';
