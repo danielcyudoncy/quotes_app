bccee81e19d1c848336ad49a3657f67618e6354a
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-08-11 21:25:27 +0100

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:quotes_app/controllers/auth_controller.dart';
@@ -10,18 +9,17 @@ import 'package:quotes_app/myApp.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
-  if(kIsWeb){
-    await Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyCAuMomqOGrMQ8WZVwZz_Vtz83ojkfPGzY",
-  authDomain: "quotes-app-8e61c.firebaseapp.com",
-  projectId: "quotes-app-8e61c",
-  storageBucket: "quotes-app-8e61c.appspot.com",
-  messagingSenderId: "223388707836",
-  appId: "1:223388707836:web:337021959ccc773d046079"));
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(apiKey: "AIzaSyCAuMomqOGrMQ8WZVwZz_Vtz83ojkfPGzY",
+    authDomain: "quotes-app-8e61c.firebaseapp.com",
+    projectId: "quotes-app-8e61c",
+    storageBucket: "quotes-app-8e61c.appspot.com",
+    messagingSenderId: "223388707836",
+    appId: "1:223388707836:web:337021959ccc773d046079"));
+
+
   Get.put(AuthController());
   Get.put(ProfileController());
-  }else{
-    await Firebase.initializeApp();
-  }
   
   
   runApp(const MyApp());
